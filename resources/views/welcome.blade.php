dc7ab1a788d790de9a9622f5614e0ccf613c77ca
CuauhtemocRmz <cuauhtemocrmz@outlook.com>
2022-07-13 12:27:30 -0500

    cambios a welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,9 +16,7 @@
 <div class="card text-center py-5">
         <h1 >Hola usuario.</h1>
         <h2 >Para usar el sistema necesita <b>Iniciar Sesion</b></h2>
-        <h2>Si aun no se registra use el siguiente enlace</h2>
-        <button class=" cta"> ewe</button>
-</div>
+        
 @endif
     </div>
 
